c256e0afbacd674777b1020b1050febb95a5ef7b
RodrigoSR1 <115092535+RodrigoSR1@users.noreply.github.com>
2023-06-26 19:30:30 -0600

    Create ing901.sql

---
+++ ing901.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE ING901;
+
+USE ING901;
+
+CREATE TABLE Personas (id INT PRIMARY KEY AUTO_INCREMENT,nombre VARCHAR(50));
+
+INSERT INTO Personas (nombre) VALUES
+  ('Rafael Alvarez Bartolo'),
+  ('Hector Jesus Bautista Patino'),
+  ('Brayan Camilo Arana'),
+  ('JOSE JESUS CARDELAS VILCHIS'),
+  ('Hiojan Geovanny Carrasco Garcia'),
+  ('Ivan De La Cruz Monfil'),
+  ('Christian Jair Espinoza Hernandez'),
+  ('Marco Antonio Garcia Garcia'),
+  ('Oscar Garcia Feliciano'),
+  ('Humberto Garcia Sanchez'),
+  ('Jorge Garcia Vega'),
+  ('Emmanuel Gomez Dominguez'),
+  ('Vicente Roberto Gomez Maciel'),
+  ('Jose Emmanuel Gomez Umbral'),
+  ('Alfonso Gutierrez Tellez'),
+  ('Eduardo Hernandez Rodriguez'),
+  ('Jonathan Aldair Leon Trejo'),
+  ('Juan Lima Arrona'),
+  ('CARLOS ALBERTO LOPEZ RODRIGUEZ'),
+  ('Ilse Maria Miguel Robledo'),
+  ('Hugo Eduardo Miranda Barrera'),
+  ('Ismael Monroy Villa'),
+  ('Gustavo Uriel Montiel Montiel'),
+  ('Ana Maria Munoz Jimenez'),
+  ('Ramiro Sánchez Lozano'),
+  ('Jonathan Sandoval Nava'),
+  ('Rodrigo Santamaria Romero'),
+  ('Cristhian Santiago Martinez'),
+  ('Miguel Angel Servach Vega');
